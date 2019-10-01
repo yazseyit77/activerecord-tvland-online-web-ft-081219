aea305511d24ab5b24d5df56f6fa522871abeff4
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-10-01 04:30:32 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -4,6 +4,6 @@ class Show < ActiveRecord::Base
   belongs_to :network
 
   def actors_list
-    self.actors.collect {|actor| "#{actor.name}"}
+    self.actors.collect {|actor| "#{actor.full_name}"}
   end
 end
